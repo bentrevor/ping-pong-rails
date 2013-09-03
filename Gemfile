e9b7c71b5ab9dcbc49717b97f88e2f75be527c3b
Ben Trevor <benjamin.trevor@gmail.com>
2013-09-03 09:58:36 -0500

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://rubygems.org'
+
+gem 'rails',        '~> 4.0.0'
+gem 'sqlite3',      '~> 1.3.8'
+gem 'jquery-rails', '~> 3.0.4'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PingPong::Application.config.secret_key_base = '9101c64710c0078a99d99536f7225ca0f39a017ba7fda8227c09d2bdb30a0c63db1c46273404eb5db415cd995d9625fc9f6fd1f35b359e1ae040bc91c81d81ee'
